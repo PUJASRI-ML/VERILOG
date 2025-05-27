9ac888a01d4dbbc9c805c9335afcf887cd53efc5
PUJASRI-ML <146067975+PUJASRI-ML@users.noreply.github.com>
2025-05-27 16:30:10 +0530

    xor gate

--- basic gates/xor.v
+++ basic gates/xor.v
@@ -15,3 +15,24 @@ endmodule
 module xor_gate(output y, input a,b);
    xor x1(y,a,b);
 endmodule
+
+//Testbench
+module xor_tb;
+reg a,b;
+wire y;
+   xor_gate dut(.a(a),.b(b),.y(y));
+initial begin
+    $display("XOR GATE TRUTH TABLE");
+   $monitor("a : %b b : %b     y : %b ",a,b,y);
+a = 0 ; b = 0 ; #10 ;
+a = 0 ; b = 1 ; #10 ;
+a = 1 ; b = 0 ; #10 ;
+a = 1 ; b = 1 ; #10 ;
+$finish;
+end
+//TO VIEW WAVEFORMS
+initial begin
+    $dumpfile("xor_gate.vcd");
+    $dumpvars(0,xor_tb);
+end
+endmodule
