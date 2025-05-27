59213e42ab843d4748dd69e06670a84cbad01031
PUJASRI-ML <146067975+PUJASRI-ML@users.noreply.github.com>
2025-05-27 16:28:19 +0530

    or gate

--- basic gates/or.v
+++ basic gates/or.v
@@ -15,3 +15,24 @@ endmodule
 module or_gate(output y, input a,b);
    or o1(y,a,b);
 endmodule
+
+//Testbench
+module or_tb;
+reg a,b;
+wire y;
+   or_gate dut(.a(a),.b(b),.y(y));
+initial begin
+  $display("OR GATE TRUTH TABLE");
+   $monitor("a : %b b : %b     y : %b ",a,b,y);
+a = 0 ; b = 0 ; #10 ;
+a = 0 ; b = 1 ; #10 ;
+a = 1 ; b = 0 ; #10 ;
+a = 1 ; b = 1 ; #10 ;
+$finish;
+end
+//TO VIEW WAVEFORMS
+initial begin
+  $dumpfile("or_gate.vcd");
+  $dumpvars(0,or_tb);
+end
+endmodule
