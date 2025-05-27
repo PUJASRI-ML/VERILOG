dabdab83e5425dcb1b6ffa53ba919ab1d61db8e5
PUJASRI-ML <146067975+PUJASRI-ML@users.noreply.github.com>
2025-05-27 16:29:24 +0530

    xnor gate

--- basic gates/xnor.v
+++ basic gates/xnor.v
@@ -15,3 +15,24 @@ endmodule
 module xnor_gate(output y, input a,b);
    xnor x1(y,a,b);
 endmodule
+
+//Testbench
+module xnor_tb;
+reg a,b;
+wire y;
+   xnor_gate dut(.a(a),.b(b),.y(y));
+initial begin
+    $display("XNOR GATE TRUTH TABLE");
+   $monitor("a : %b b : %b     y : %b ",a,b,y);
+a = 0 ; b = 0 ; #10 ;
+a = 0 ; b = 1 ; #10 ;
+a = 1 ; b = 0 ; #10 ;
+a = 1 ; b = 1 ; #10 ;
+$finish;
+end
+//TO VIEW WAVEFORMS
+initial begin
+    $dumpfile("xnor_gate.vcd");
+    $dumpvars(0,xnor_tb);
+end
+endmodule
