c27218dd7edba71d33f213655d5aa6b1d21c61cc
PUJASRI-ML <146067975+PUJASRI-ML@users.noreply.github.com>
2025-05-27 16:17:15 +0530

    nand gate

--- basic gates/nand.v
+++ basic gates/nand.v
@@ -15,3 +15,24 @@ endmodule
 module nand_gate(output y, input a,b);
     nand n1(y,a,b);
 endmodule
+
+//Testbench
+module and_tb;
+reg A,B;
+wire out_and;
+and_gate dut(.A(A),.B(B),.out_and(out_and));
+initial begin
+$display("AND GATE TRUTH TABLE");
+$monitor("A : %b B : %b     OUT : %b ",A,B,out_and);
+A = 0 ; B = 0 ; #10 ;
+A = 0 ; B = 1 ; #10 ;
+A = 1 ; B = 0 ; #10 ;
+A = 1 ; B = 1 ; #10 ;
+$finish;
+end
+//TO VIEW WAVEFORMS
+initial begin
+$dumpfile("and_gate.vcd");
+$dumpvars(0,and_tb);
+end
+endmodule
